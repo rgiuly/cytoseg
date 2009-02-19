34daf712dde0f803980d167599715b078575981a
rgiuly <rgiuly@07e81496-a9c8-11dd-91bf-632da8b6e10b>
2009-02-19 23:19:39 +0000

    added pixel classification

--- cytoseg/findBlobs.m
+++ cytoseg/findBlobs.m
@@ -3,6 +3,9 @@ function [centroids, areas] = findBlobs(volume1D,d1,d2,d3,threshold,showResults)
     % dimensions d1,d2,d3 are the dimensions of the original 3D array.
     % a 1D array is used because mlabwrap currently does not support 3D
     % arrays.
+
+	areas = [];
+	centroids = [];
     
     volume = reshape(volume1D, d1,d2,d3);
     if showResults
@@ -14,6 +17,7 @@ function [centroids, areas] = findBlobs(volume1D,d1,d2,d3,threshold,showResults)
     	%cytoseg_sliceView(labelMatrix);
     end
     props = regionprops(labelMatrix,{'Area', 'Centroid'});
+  	display(sprintf('findBlobs.m in matlab: number of connected components: %d', length(props)))
     for i=1:length(props)
         areas(i) = props(i).Area;
         centroids(i,:) = props(i).Centroid;
