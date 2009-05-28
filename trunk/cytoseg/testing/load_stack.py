cad0d9e18bb794dae0c23ee3bcdcbf729f63d0f3
rgiuly <rgiuly@07e81496-a9c8-11dd-91bf-632da8b6e10b>
2009-05-28 14:43:15 +0000

    add

--- trunk/cytoseg/testing/load_stack.py
+++ trunk/cytoseg/testing/load_stack.py
@@ -3,6 +3,8 @@ sys.path.append("..")
 
 from cytoseg_classify import *
 
+temporaryDataFolder = "/tmp/cytoseg_data"
+
 app = wx.PySimpleApp()
 frm = ClassificationControlsFrame(makeClassifyGUITree())
 frm.Show()
