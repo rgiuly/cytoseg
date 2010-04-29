599271635311daf4575e7857bde6c5d776a86ca4
rgiuly <rgiuly@07e81496-a9c8-11dd-91bf-632da8b6e10b>
2010-04-29 02:44:31 +0000

    commit

--- trunk/cytoseg/testing/RUN_MPI_sample_70slice_small.sh
+++ trunk/cytoseg/testing/RUN_MPI_sample_70slice_small.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Num_of_processors=20
+Num_of_processors=2
 
 #mpirun -np $Num_of_processors python machine_learning_command.py /home/demo/data/machine_learning_tutorial/b /home/demo/data/machine_learning_tutorial/a /home/demo/data/machine_learning_tutorial/a_membranes /home/demo/temp /home/demo/temp
 
-mpirun -np $Num_of_processors python sbfsem_batch_mpi.py /home/demo/data/neuropil-read-only/input /home/demo/data/neuropil-read-only/data /home/demo/data/neuropil-read-only/seg /home/demo/temp /tmp
+mpirun -np $Num_of_processors python sbfsem_batch_mpi.py /home/demo/data/neuropil-read-only/input/40x40 /home/demo/data/neuropil-read-only/data/40x40 /home/demo/data/neuropil-read-only/seg/40x40 /home/demo/temp /home/demo/temp
 
