b28fe87533a9c04e9e031069379aa1ad42d552b9
rgiuly <rgiuly@07e81496-a9c8-11dd-91bf-632da8b6e10b>
2010-05-02 06:52:35 +0000

    commit

---
+++ trunk/cytoseg/testing/RUN.sh
@@ -0,0 +1,4 @@
+PATH=/home/demo/bin:/usr/local/cglX/bin/:/home/demo/epd-6.1-1-rh5-x86_64/bin:$PATH
+LD_LIBRARY_PATH=$SAGE_DIRECTORY/lib:/usr/local/cglX/lib/:/usr/local/lib64:/usr/local/lib:/home/demo/epd-6.1-1-rh5-x86_64/lib/python2.6/site-packages/orange:/home/demo/epd-6.1-1-rh5-x86_64/lib/:$LD_LIBRARY_PATH
+./RUN_MPI_sample_70slice.sh
+
