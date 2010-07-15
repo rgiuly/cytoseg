13ce866a0f5e09bd1720dfc21f6dca651ec08655
rgiuly <rgiuly@07e81496-a9c8-11dd-91bf-632da8b6e10b>
2010-07-15 22:34:10 +0000

    commit

---
+++ trunk/cytoseg/testing/run_sample_cluster0.bat
@@ -0,0 +1,5 @@
+rem #!/bin/bash
+
+rem Num_of_processors=10
+
+python ..\sbfsem_batch_mpi.py o:\images\neuropil\data o:\images\neuropil\data o:\images\neuropil\seg o:\temp o:\cytoseg_data sbfsem_settings1.py
