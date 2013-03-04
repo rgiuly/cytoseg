0cbc324a99c21e45aca55a69e8f31854e0b8ac8a
rgiuly <rgiuly@07e81496-a9c8-11dd-91bf-632da8b6e10b>
2013-03-04 23:53:00 +0000

    commit

---
+++ cytoseg/testing/is_blank.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import sys
+
+image = Image.open(sys.argv[1])
+#image.show()
+
+(w, h) = image.size
+
+allZeros = True
+
+print "Testing if image is blank"
+pixels = image.load()
+for x in range(w):
+    for y in range(h):
+        value = pixels[x, y]
+        if value != 0:
+            allZeros = False
+
+print allZeros
+
+if allZeros:
+    print "Using exit status 1, the image is blank"
+    exit(1)
+else:
+    print "Using exit status 0, the image is not blank"
+    exit(0)
+
+
+# References
+# http://stackoverflow.com/questions/2181292/using-the-image-point-method-in-pil-to-manipulate-pixel-data
+
